23ce372b1e8e3a3aa3709e347d966070708c7593
AnnaMarcinkevich <anna.martsynkevich2020@gmail.com>
2021-03-28 12:47:29 +0300

    fix: rename class in BEM-notation according to rule 3.1 in html-and-css.md

--- clean-code-s1e1/app.js
+++ clean-code-s1e1/app.js
@@ -85,11 +85,11 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".task-list__btn-edit");
-    var containsClass=listItem.classList.contains("list__item_edit-mode");
-    //If class of the parent is .list__item_edit-mode
+    var containsClass=listItem.classList.contains("task-list__item_edit-mode");
+    //If class of the parent is .task-list__item_edit-mode
     if(containsClass){
 
-        //switch to .list__item_edit-mode
+        //switch to .task-list__item_edit-mode
         //label becomes the inputs value.
         label.innerText=editInput.value;
         editBtn.innerText="Edit";
@@ -99,7 +99,7 @@ var editTask=function(){
     }
 
     //toggle .edit-mode on the parent.
-    listItem.classList.toggle("list__item_edit-mode");
+    listItem.classList.toggle("task-list__item_edit-mode");
 };
 
 
